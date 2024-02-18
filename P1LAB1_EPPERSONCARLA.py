de3edf2a3ea5ef26d0b2e56b9800d8fda1f8cec8
EppersonCarla <160422122+EppersonCarla@users.noreply.github.com>
2024-02-18 13:15:09 -0500

    Add files via upload
    First Submission P1LAB1

---
+++ P1LAB1_EPPERSONCARLA.py
@@ -0,0 +1,11 @@
+"""
+Name: Carla Epperson
+Date: February 18, 2024
+Assignment Name: P1LAB1
+Use Python's input and print functions to complete first program
+"""
+
+first_name = input('Enter your first name:')
+print('Hey', first_name)
+print('Welcome to zyBooks!')
+
