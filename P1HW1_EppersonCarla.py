f8f5fa8c4c09c8946a7b6bc972fd4aefdb6e504c
EppersonCarla <160422122+EppersonCarla@users.noreply.github.com>
2024-02-25 18:06:03 -0500

    Add files via upload

---
+++ P1HW1_EppersonCarla.py
@@ -0,0 +1,16 @@
+# Carla Epperson
+
+# 02/25/2024
+
+# P1LAB2
+
+# LAB: Warm up: Basic output with variables
+
+num1 = int(input("Enter integer:\n"))
+print("You entered:", num1)
+print(num1, "squared is", num1 ** 2)
+print("And", num1, "cubed is", num1 ** 3, "!!")
+
+num2 = int(input("Enter another integer:\n"))
+print(num1, "+", num2, "is", num1 + num2)
+print(num1, "*", num2, "is", num1 * num2)
